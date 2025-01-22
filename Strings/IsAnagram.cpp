5c15ede7be0862fbf39d3303672881d859faf245
Avjot Singh <119579554+Asingh-2430@users.noreply.github.com>
2025-01-22 10:44:08 +0530

    Is Anagram problem in C++

---
+++ Strings/IsAnagram.cpp
@@ -0,0 +1,30 @@
+ 
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // If the lengths of the strings are different, they can't be anagrams.
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        // Create a frequency count vector for 26 letters of the alphabet.
+        vector<int> count(26, 0);
+        
+        // Traverse both strings, incrementing count for characters in `s`
+        // and decrementing count for characters in `t`.
+        for (int i = 0; i < s.length(); ++i) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+        
+        // If the frequency count is zero for all characters, the strings are anagrams.
+        for (int i = 0; i < 26; ++i) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
