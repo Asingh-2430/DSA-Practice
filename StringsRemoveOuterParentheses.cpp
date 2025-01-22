51b1822a30bcfe5532691bd26229d1b21f8b3cad
Avjot Singh <119579554+Asingh-2430@users.noreply.github.com>
2025-01-22 10:30:28 +0530

    Remove Outer Parentheses problem

---
+++ StringsRemoveOuterParentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int size = s.length();
+        int count = 0;
+        string r = "";
+
+        for (int i = 0; i < size; i++) {
+            if (s[i] == '(') {
+                count++;
+            }
+            if (s[i] == ')') {
+                count--;
+            }
+            if (s[i] == '(' && count == 1 || s[i] == ')' && count == 0) {
+                continue;
+            } else {
+                r += s[i];
+            }
+        }
+        return r;
+    }
+};
+/* The best method to do this question is by doing on a copy.
+   We will analyze the values where the first and last parentheses are
+   and then consider the values accordingly. */
