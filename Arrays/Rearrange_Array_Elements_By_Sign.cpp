28321af559e1e2b798c0de7041b31760357d646d
Avjot Singh <119579554+Asingh-2430@users.noreply.github.com>
2025-02-06 10:51:08 +0530

    Rearrange_Array_Elements_By_Sign.cpp

--- Arrays/Rearrange_Array_Elements_By_Sign.cpp
+++ Arrays/Rearrange_Array_Elements_By_Sign.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i += 2) {
+            if (nums[i] > 0) {
+                continue;
+            } else {
+                for (int j = 0; j < nums.size(); j++) {
+                    if (nums[j] >= 0 && j % 2 == 1) { // Ensure j is at an odd index to maintain the alternating pattern
+                        swap(nums[i], nums[j]);
+                        break; // Exit inner loop after swapping
+                    }
+                }
+            }
+        }
+        return nums; // Return after processing all elements
+    }
+};
